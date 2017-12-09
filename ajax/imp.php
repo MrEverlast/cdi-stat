59fd24f8cb368f7d940514aa1ce214ffd9a1b393
Max Brooks <max.brooks83gmail.com>
2017-12-09 21:29:47 +0100

    Edit - Setting (css)

--- ajax/imp.php
+++ ajax/imp.php
@@ -1,10 +1,10 @@
-<div id="modal_year" class="ui modal" data-page="0">
+<div id="modal_year" class="ui modal" data-page="0" style="height: 80%;">
 
-  <div class="content scrolling">
+  <div class="content scrolling" style="max-height: calc(100% - 64px);">
     <?php include_once $_DIR.'/ajax/imp/modal0.php'; ?>
   </div>
 
-  <div class="actions">
+  <div class="actions" style="bottom: 0;width: 100%;position: absolute;">
     <button class="ui button cancel red"><i class="icon delete"></i>Annuler</button>
     <button id="previous" class="ui button disabled"><i class="icon chevron left"></i>Précédent</button>
     <button id="next" class="ui button disabled">Suivant<i class="icon chevron right"></i></button>
