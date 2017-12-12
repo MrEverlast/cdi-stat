a472256c2d2c72a83e8d0b74942dfcbccd85acba
Nicolas ANDRES <98andresnicolas@gmail.com>
2017-12-12 08:24:31 +0100

    Edit- créer eleve code postal DEBUG

--- ajax/req/elv/create.php
+++ ajax/req/elv/create.php
@@ -1,12 +1,12 @@
 <?php 
 	$last_name = $_POST['last_name'];
 	$first_name = $_POST['first_name'];
-	$postal_code = $_POST['postal_code'];
+	$post_code = $_POST['post_code'];
 	$city = $_POST['city'];
 	$id_classe = $_POST['id_classe'];
 
 	$query = "INSERT INTO `t_eleve` (`last_name`,`first_name`,`city`,`post_code`,`id_division`) 
-			  VALUES ('$last_name', '$first_name', '$city' , '$postal_code' ,'$id_classe')";
+			  VALUES ('$last_name', '$first_name', '$city' , '$post_code' ,'$id_classe')";
 
 	$bdd->requeteBDD($query);
 
