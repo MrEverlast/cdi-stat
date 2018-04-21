ff33a0682e32624e54155080bc71c6398587dc6e
Max Brooks <max.brooks83gmail.com>
2018-04-21 14:46:15 +0200

    Stats not finished

--- ajax/stats/req/elv_inscrit_test.php
+++ ajax/stats/req/elv_inscrit_test.php
@@ -6,8 +6,9 @@ $diff = $_POST['diff'];
 $date = $_POST['date'];
 
 
-for ($i=0; $i < $diff; $i++) {
-  $req = "SELECT COUNT(`id`) AS nb FROM `t_registration` WHERE `date_create` = DATE_ADD('$date', INTERVAL $i DAY)";
+for ($i=0; $i <= $diff; $i++) {
+  $req = "SELECT COUNT(`id`) AS nb FROM `t_registration` WHERE `date_create` BETWEEN DATE_ADD('$date', INTERVAL $i DAY) AND DATE_ADD('$date 23:59:59', INTERVAL $i DAY)";
+  // "SELECT COUNT(`id`) AS nb FROM `t_registration` WHERE `date_create` BETWEEN '2018-04-21' AND '2018-04-21 23:59:59'";
   $query = $bdd->requeteBDD($req);
   $data = $query->fetch();
   $tabData[$i+1] = $data['nb'];
--- pages/admin_stats.php
+++ pages/admin_stats.php
@@ -5,19 +5,18 @@
 
 <div class="ui grid padded">
   <div class="row">
-    <div class="col">
+    <div class="column" style="width: 306px;">
       <h3>Debut</h3>
       <div class="ui calendar" id="calendar1"></div>
       <br>
       <h3>Fin</h3>
       <div class="ui calendar" id="calendar2"></div>
     </div>
-    <div class="col">
+    <div class="column thirteen wide">
       <canvas id="myChart"></canvas>
     </div>
   </div>
 </div>
-
 
 
 <script>
@@ -72,25 +71,37 @@ function loadCalender(id, initialDate = getCalendarEdge()) {
     monthFirst: false,
     endCalendar: endCalendar,
     startCalendar: startCalendar,
-    onChange: function() {
-      $('#calendar1').calendar('refresh');
-      $('#calendar2').calendar('refresh');
+    onChange: function(date) {
+      var calendar1 = $('#calendar1');
+      var calendar2 = $('#calendar2');
 
-      var date1 = $('#calendar1').calendar('get date');
-      var date2 = $('#calendar2').calendar('get date');
+      calendar1.calendar('refresh');
+      calendar2.calendar('refresh');
 
-      if (verifDates(date1, date2)) {
-        $.ajax({
-          method: 'POST',
-          url: '/ajax/stats/req/elv_inscrit_test.php',
-          data: {
-            diff: getDayDiff(moment(date1), moment(date2)),
-            date: date1
-          },
-          success: function(data) {
-            console.log(data);
-          }
-        });
+      var date1;
+      var date2;
+      if (calendar2.calendar('get date') !== null || this.id === 'calendar2') {
+        if (this.id === 'calendar1') {
+          date1 = moment(date).format('YYYY-MM-DD');
+          date2 = moment(calendar2.calendar('get date')).format('YYYY-MM-DD');
+        } else if (this.id === 'calendar2') {
+          date1 = moment(calendar1.calendar('get date')).format('YYYY-MM-DD');
+          date2 = moment(date).format('YYYY-MM-DD');
+        }
+
+        if (verifDates(date1, date2)) {
+          $.ajax({
+            method: 'POST',
+            url: '/ajax/stats/req/elv_inscrit_test.php',
+            data: {
+              diff: getDayDiff(moment(date1), moment(date2)),
+              date: date1
+            },
+            success: function(data) {
+              loadGraph(data, date1, date2);
+            }
+          });
+        }
       }
       
     }
@@ -105,12 +116,86 @@ $('#calendar1').calendar('set date', minDate, updateInput = false, fireChange = 
 $('#calendar2').calendar('set date', maxDate, updateInput = false, fireChange = true);
 
 function getDayDiff(date1, date2) {
-  console.log(Math.abs(date1.diff(date2.format(),'days')));
+  //console.log(Math.abs(date1.diff(date2.format(),'days')));
   return Math.abs(date1.diff(date2.format(),'days'));
 }
 
 function verifDates(date1, date2) {
-  return moment(date1).isBefore(date2);
+  //console.log(moment(date1).isBefore(date2) || moment(date1).isSame(date2));
+  return moment(date1).isBefore(date2) || moment(date1).isSame(date2);
 }
+
+function loadGraph(_data, date1, date2) {
+  var json = JSON.parse(_data);
+  var ctx = $("#myChart");
+
+  var title = moment(date1).format('D MMM YYYY') + ' à ' + moment(date2).format('D MMM YYYY'); 
+  if (window.myLineChart)
+      window.myLineChart.destroy(); 
+  
+  window.myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Élèves',
+            data: [],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+				responsive: true,
+				title: {
+					display: true,
+					text: title
+				},
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+				},
+				hover: {
+					mode: 'nearest',
+					intersect: true
+				},
+				scales: {
+					xAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Date'
+						}
+					}],
+					yAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Nombre'
+						}
+					}]
+				}
+			}
+  });
+  
+  var arr = $.map(json, function(el) { return el });
+
+  arr.forEach((element, index) => {
+    addData(window.myLineChart, moment(date1).add(index, 'days').format('D MMM'), element);
+  });
+  window.myLineChart.update();
+}
+
+function addData(chart, label, data) {
+  chart.data.labels.push(label);
+  chart.data.datasets.forEach((dataset) => {
+    dataset.data.push(data);
+  });
+}
+
 
 </script>
